d252c77496cda52f70dc98beb9f73792719e96f7
SVolochandrey <volochan.andrey@gmail.com>
2023-05-21 10:41:00 +0300

    +

--- JS/MoreLessBtns.js
+++ JS/MoreLessBtns.js
@@ -1,12 +1,42 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   var button = document.querySelector(".read-more-button");
+//   var description = document.querySelector(".section-summary__descr");
+//   var isExpanded = false;
+//   var descriptionHeight = "150px";
+//   var scrollOffset = 20;
+
+//   description.style.maxHeight = descriptionHeight;
+//   // description.style.overflow = "hidden";
+
+//   button.addEventListener("click", function () {
+//     if (isExpanded) {
+//       description.style.maxHeight = descriptionHeight;
+//       button.textContent = "More";
+//       var descriptionOffset =
+//         description.getBoundingClientRect().top + window.pageYOffset;
+//       window.scrollTo({
+//         top: descriptionOffset - scrollOffset,
+//         behavior: "smooth",
+//       });
+//     } else {
+//       description.style.maxHeight = description.scrollHeight + "px";
+//       button.textContent = "Less";
+//     }
+
+//     isExpanded = !isExpanded;
+//   });
+// });
+
+
 document.addEventListener("DOMContentLoaded", function () {
   var button = document.querySelector(".read-more-button");
   var description = document.querySelector(".section-summary__descr");
   var isExpanded = false;
   var descriptionHeight = "150px";
+  var descriptionHeightWide = "300px"; // Новая высота для ширины >= 1200px
   var scrollOffset = 20;
 
   description.style.maxHeight = descriptionHeight;
-  // description.style.overflow = "hidden";
 
   button.addEventListener("click", function () {
     if (isExpanded) {
@@ -25,4 +55,20 @@ document.addEventListener("DOMContentLoaded", function () {
 
     isExpanded = !isExpanded;
   });
+
+  // Медиа-запрос для изменения максимальной высоты при ширине >= 1200px
+  var mediaQuery = window.matchMedia("(min-width: 1200px)");
+
+  function handleMediaQuery(mediaQuery) {
+    if (mediaQuery.matches) {
+      descriptionHeight = descriptionHeightWide;
+      description.style.maxHeight = descriptionHeight;
+    } else {
+      descriptionHeight = "150px";
+      description.style.maxHeight = descriptionHeight;
+    }
+  }
+
+  handleMediaQuery(mediaQuery); // Обрабатываем медиа-запрос при загрузке страницы
+  mediaQuery.addListener(handleMediaQuery); // Обрабатываем медиа-запрос при изменении размера окна
 });
